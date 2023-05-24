4029f16092cfa5ef35d0ef5b2aada9275af03a32
santigarciam <sangarcia@itba.edu.ar>
2023-05-24 18:31:57 -0300

    test fail

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ describe('GET /', () => {
   it('should return "Hello World from Docker and Node.js!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).to.equal(200);
-    expect(response.text).to.include('<h1>Hello World from Docker and Node.js!</h1>');
+    expect(response.text).to.include('<h1>Hello W!orld from Docker and Node.js!</h1>');
   });
 });
