f5a7e3f33ad17a25093510e3e1a7448bc4054c57
tom <tcerdeira@itba.edu.ar>
2023-05-27 21:54:55 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World from Docker and Node.js!</h1>');
+    res.send('<h1>Hello ton from Docker and Node.js!</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
