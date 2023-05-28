e28e0428a1eb21d33f898c2fad4eb76a8181089b
tom <tcerdeira@itba.edu.ar>
2023-05-27 21:59:27 -0300

    .

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('GET /', () => {
-  it('should return "Hello World from Docker and Node.js!"', async () => {
+  it('should return "Hello Mundo from Docker and Node.js!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).to.equal(200);
-    expect(response.text).to.include('<h1>Hello World from Docker and Node.js!</h1>');
+    expect(response.text).to.include('<h1>Hello Mundo from Docker and Node.js!</h1>');
     // expect(response.text).to.include('This should fail Jenkins build! ');
   });
 });
